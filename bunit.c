0e0234ffe3b0c9876331143b52ee51958b7fb75a
NandOS <luis.santo@ufms.br>
2024-12-12 18:50:34 -0400

    Add files via upload

--- bunit.c
+++ bunit.c
@@ -38,21 +38,6 @@ int matrix() {
     return 0;
 }
 
-#define INCHES_PER_POUND 166
 
-int box() {
-    int height, length, width, volume, weight;
-    printf("Enter height of box:  ");
-    scanf("%d", &height);
-    printf("Enter the length of the box:  ");
-    scanf("%d", &length);
-    printf("Enter the width of box:  ");
-    scanf("%d", &width);
-    volume = height * length * width;
-    weight = (volume +165)/ INCHES_PER_POUND;
 
-    printf("Volume(cubic inches): %d\n", volume);
-    printf("Dimensional Weight %d\n", weight);
 
-    return 0;
-}
