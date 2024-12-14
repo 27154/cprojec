20953f4e314a1a824892da09dc9f0b6cae5fbef4
NandOS <luis.santo@ufms.br>
2024-12-14 13:13:39 -0400

    Add files via upload

--- diceGame.c
+++ diceGame.c
@@ -7,46 +7,70 @@
 #include <unistd.h>
 #include <windows.h>
 
-int myrandom(int max) {
+
+#define CUSTOM_EXIT(message) do { printf("%s\n", message); exit(1); } while(0)
+
+int myrandom(int max)
+{
     int x;
-    x = rand() % max - 1;
+    x = rand() % max;
     return x;
 }
 
-    int main() {
+int main() {
     int balance, bet, guess, dice;
+    int choice;
 
     srand(getpid());
-
     balance = 1000;
-    while (1) {
-        printf(" make your bet, please:");
+
+    while (1)
+    {
+        printf("Menu\n");
+        printf("1. Bet\n");
+        printf("2. Quit\n");
+        printf("Chose an option: ");
+        fflush(stdout);
+        scanf("%d", &choice);
+
+        if (choice == 2) {
+            CUSTOM_EXIT("You decided to quit:");
+        } else if (choice != 1)
+        {
+            printf("Try again!");
+            continue;
+        }
+        printf("Make your bet, please:");
         fflush(stdout);
         scanf("%d", &bet);
 
-        printf("Make your guess (1-6), zero means quit");
+        printf("Make your choice (1-6), zero means out:");
         scanf("%d", &guess);
-        if (guess == 0) {
-            return 0;
+        if (guess == 0)
+        {
+            CUSTOM_EXIT("You quit from the game betting nothing!");
+
         }
-        printf("Throwing the dice...\n");
+        printf("Throwing the dice...");
         sleep(2);
-        dice = myrandom(6);
-        printf(" Dice results: %d\n", dice);
+        dice = myrandom(6) +1;
+        printf("Dice results: %d\n", dice);
 
-        if ( guess != dice) {
-            balance = balance - bet;
-            printf("Mother Fortune is not at your back! Bet again!\n");
+        if (guess != dice)
+        {
+            balance -= bet;
+            printf("Apparently, it's not your lucky day\n");
 
-        } else   {
+        } else
+        {
             bet *= 3;
             balance += bet;
-            printf("It's your lucky day your son of a bitch!\n");
+            printf("Today is your lucky day\n");
         }
 
-        if (balance == 0) {
-            printf("You lost all your money\n");
-            return 0;
+        if (balance < 0 )
+        {
+            printf("You lost all your money, however you can keep bettin'");
         }
         printf("Balance: $%d\n", balance);
     }
